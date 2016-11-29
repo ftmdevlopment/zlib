5954137e88ce5b76c597a825e5e5cdffceeeb72f
Siwei Xu <xusiwei1236@163.com>
2016-11-29 21:50:26 +0800

    fix ndk-build failed

--- Android.mk
+++ Android.mk
@@ -1,9 +1,4 @@
 LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-
-# measurements show that the ARM version of ZLib is about x1.17 faster
-# than the thumb one...
-LOCAL_ARM_MODE := arm
 
 zlib_files := \
 	src/adler32.c \
@@ -22,15 +17,6 @@ zlib_files := \
 	src/uncompr.c \
 	src/zutil.c
 
-LOCAL_MODULE := libz
-LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -O3 -DUSE_MMAP
-LOCAL_SRC_FILES := $(zlib_files)
-ifeq ($(TARGET_ARCH),arm)
-  LOCAL_SDK_VERSION := 9
-endif
-include $(BUILD_SHARED_LIBRARY)
-
 include $(CLEAR_VARS)
 
 LOCAL_ARM_MODE := arm
@@ -43,28 +29,6 @@ ifeq ($(TARGET_ARCH),arm)
 endif
 include $(BUILD_STATIC_LIBRARY)
 
-include $(CLEAR_VARS)
-
-LOCAL_ARM_MODE := arm
-LOCAL_MODULE := libz
-LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -O3 -DUSE_MMAP
-LOCAL_SRC_FILES := $(zlib_files)
-LOCAL_MULTILIB := both
-include $(BUILD_HOST_STATIC_LIBRARY)
-
-
-include $(CLEAR_VARS)
-
-LOCAL_ARM_MODE := arm
-LOCAL_MODULE := libz-host
-LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -O3 -DUSE_MMAP
-LOCAL_SRC_FILES := $(zlib_files)
-LOCAL_MULTILIB := both
-include $(BUILD_HOST_SHARED_LIBRARY)
-
-
 
 # libunz used to be an unzip-only subset of libz. Only host-side tools were
 # taking advantage of it, though, and it's not a notion supported by zlib
@@ -72,13 +36,6 @@ include $(BUILD_HOST_SHARED_LIBRARY)
 # up needing to drag in most of the other files anyway. So this is a first
 # step towards killing libunz. If you're reading this in the K release or
 # later, please see if you can get a bit further in removing libunz...
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := $(zlib_files)
-LOCAL_MODULE:= libunz
-LOCAL_ARM_MODE := arm
-LOCAL_MULTILIB := both
-include $(BUILD_HOST_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(zlib_files)
@@ -96,17 +53,7 @@ LOCAL_SRC_FILES:=        \
 
 LOCAL_MODULE:= gzip
 
-LOCAL_SHARED_LIBRARIES := libz
+LOCAL_STATIC_LIBRARIES := libz
 
 include $(BUILD_EXECUTABLE)
 
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES:=        \
-	src/test/minigzip.c
-
-LOCAL_MODULE:= minigzip
-
-LOCAL_STATIC_LIBRARIES := libz
-
-include $(BUILD_HOST_EXECUTABLE)
